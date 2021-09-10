4b0615392f90fc1d307f70a3843f2af778d69718
shaad1934B <86871454+shaad1934B@users.noreply.github.com>
2021-09-10 17:34:11 +0600

    Create thsbd.py
    https://github.com/shaad1934B/thsbd.git
    ls
    cd thsbd
    ls
    python2 thsbd

---
+++ thsbd.py
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+lib = File.expand_path('lib', __dir__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'trace_location/version'
+
+Gem::Specification.new do |s|
+  s.name          = 'trace_location'
+  s.version       = TraceLocation::VERSION
+  s.authors       = ['Yoshiyuki Hirano', 'Misaki Shioi']
+  s.email         = ['mdshaad145@me.com', 'mdshaad145@gmail.com']
+  s.homepage      = 'https://github.com/shaad1934B/trace_location'
+  s.summary       = 'helps you get tracing the source location of codes'
+  s.description   = %(TraceLocation helps you get tracing the source location of codes, and helps you can get reading the huge open souce libraries in Ruby)
+  s.license       = 'MIT'
+  s.files         = Dir.chdir(File.expand_path('.', __dir__)) do
+    `git ls-files -z`.split("\x0").reject do |f|
+      f.match(%r{^(test|spec|features)/})
+    end
+  end
+  s.bindir        = 'exe'
+  s.executables   = s.files.grep(%r{^exe/}) { |f| File.basename(f) }
+  s.require_paths = ['lib']
+  s.required_ruby_version = '>= 2.6.0'
+
+  s.add_dependency 'method_source'
+
+  s.add_development_dependency 'bundler'
+  s.add_development_dependency 'rake'
+  s.add_development_dependency 'rspec'
+end
